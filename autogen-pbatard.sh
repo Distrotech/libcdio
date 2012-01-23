296327cfdef93683fc0f6330ddc4fece2db1cfea
Pete Batard <pbatard@gmail.com>
2012-01-23 11:28:07 +0000

    [internal] Add autogen-pbatard.sh to limit scope
    * Only image extraction and C is being tested for now

---
+++ autogen-pbatard.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+# Additional options go to configure.
+
+echo "Rebuilding ./configure with autoreconf..."
+autoreconf -f -i
+if [ $? -ne 0 ]; then
+  echo "autoreconf failed"
+  exit $?
+fi
+
+./configure --enable-maintainer-mode --disable-cddb \
+  --disable-vcd-info --disable-cxx --disable-cpp-progs \
+  --without-cd-drive --without-cd-info --without-cd-paranoia \
+  --without-cdda-player --without-cd-read "$@"
